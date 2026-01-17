aa3e4d9ed2b65fafb2e0e4b0f40ddf9e6771999e
sserges <agounkeserge@gmail.com>
2026-01-16 20:05:47 -0700

    Random Name v1

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -7,10 +7,15 @@
 #include "String.h"
 #include "Person.h"
 
+// Generate a random name
+std::string RandomName();
+
 int main()
 {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     _CrtSetBreakAlloc(-1);
+
+    srand(unsigned int(time(nullptr)));
 
 #if 0
     Queue<int> queue;
@@ -31,10 +36,27 @@ int main()
 #endif
 
 #if 1
-    Person p("Bill", 42);
-    p.Display();
+   
+    for (int i = 0; i < 20; i++)
+        std::cout << RandomName() << '\n';
+
 #endif
     std::cout << "\n\n";
     system("pause");
     return 0;
+}
+
+// Generate a random name
+std::string RandomName() {
+    std::string name;
+    int len = rand() % 6 + 3; // 3 - 8 value
+
+    // Fill in the name
+    for (int i = 0; i < len; i++)
+        name += 'a' + rand() % 26; // Add a random letter to the string
+
+    // Uppercase the first letter
+    name[0] = toupper(name[0]);
+
+    return name;
 }
